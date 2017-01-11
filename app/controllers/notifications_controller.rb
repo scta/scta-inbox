7b323d2b66827ebfbf0503abefd67406f6856f4e
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2017-01-11 05:44:19 -0500

    re-added cors header and changed post return status to 201

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,19 @@
 class NotificationsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  
+  before_filter :allow_cors
+
+  def allow_cors
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+    headers["Access-Control-Allow-Headers"] =
+      %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
+
+    head(:ok) if request.request_method == "OPTIONS"
+    # or, render text: ''
+    # if that's more your style
+  end
+
+
   def temp
     render plain: "a ldn inbox for scta resources"
   end
@@ -16,11 +29,10 @@ class NotificationsController < ApplicationController
     render :json => notifications
   end
   def create
-
     body = JSON.parse(request.body.read.html_safe)
     params = {object: body["body"], target: body["target"], updated: body["updated"]}
     @notification = Notification.create(params)
-    render plain: "Thanks for sending a POST request"
+    render plain: "Thanks for sending a POST request", status: 201
   end
   def show
     notification = Notification.find(params[:id])
