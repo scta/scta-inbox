65d84e7a11dadc6f859433c96379ea1dc47b0e9b
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2017-01-10 20:09:41 -0500

    updated last dbmigrate

--- db/migrate/20161123163918_change_data_typeto_notification.rb
+++ db/migrate/20161123163918_change_data_typeto_notification.rb
@@ -1,5 +1,5 @@
 class ChangeDataTypetoNotification < ActiveRecord::Migration[5.0]
   def change
-    change_column :notifications, :same_as,  :json
+    change_column :notifications, :same_as, 'json USING CAST(notifications AS json)'
   end
 end
