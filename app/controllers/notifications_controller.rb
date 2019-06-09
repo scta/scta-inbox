8e6ffd931113dc3176e48309f1e2066838a1d2ab
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2019-06-08 20:21:59 -0400

    added cors on create route

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -7,7 +7,11 @@ class NotificationsController < ApplicationController
     response.set_header("Host", "#{request.host}")
     response.set_header("Allow", "GET, HEAD, OPTIONS, POST")
     response.set_header("Accept-Post", "application/ld+json, text/turtle")
+
     response.set_header("Access-Control-Allow-Origin", "*")
+    response.set_header("Access-Control-Allow-Methods", %w{GET POST PUT DELETE}.join(","))
+    response.set_header("Access-Control-Allow-Headers", %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(","))
+    
     render plain: "Thanks for sending an #{request.request_method} request"
   end
   def index
